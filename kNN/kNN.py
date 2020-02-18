edb709594d88f96b64274b0ff86a4bfee17308ba
Rob van Gastel <robvangastel@hotmail.com>
2020-02-18 14:14:30 +0100

    Add single instance predict

--- kNN/kNN.py
+++ kNN/kNN.py
@@ -1,4 +1,47 @@
+import numpy as np
+from kNN.distance import euclidian
+
 class kNN:
 
-    def __init__(self):
+    def __init__(self, X, y):
+        # Training data
+        self.X = X
+        self.y = y
+        self.n_classes = len(list(set(self.y)))
+
+    def evaluate(self, X_test, y_test, k=5, d=euclidian):
+
+        # TODO: Same as predict but iterating over test set
+        pass
+
+    def predict(self, X_j, y_j, k=5, dist=euclidian):
+        distances = {}
+
+        for i in range(0, len(self.X)):
+            di = 0
+            for x, y in zip(self.X[i], X_j):
+                di += dist(X=x, Y=y)
+            distances[i] = (di, int(self.y[i]))
+
+        distances = sorted(distances.items(), key=lambda x: x[1])
+
+        # Look at the k neighbors
+        distances = distances[:k]
+
+        # Majority vote on X_j
+        return self.__majority_vote(distances)
+
+    def __majority_vote(self, neighbors):
+        votes = [0] * self.n_classes
         
+        for n in neighbors:
+            votes[n[1][1]] += 1
+        
+        # TODO: Takes first occurence if even numbers
+        return votes.index(max(votes))
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import numpy as np
+from kNN.kNN import kNN
 
-train = np.loadtxt(r'../data/MNIST_train_small.csv', delimiter=',')
+train = np.loadtxt(r'./data/MNIST_train_small.csv', 
+    delimiter=',')
+test = np.loadtxt(r'./data/MNIST_test_small.csv', 
+    delimiter=',')
+
 X_train, y_train = train[:,1:], train[:,0]
-test = np.loadtxt(r'../data/MNIST_test_small.csv', delimiter=',')
+
+
+# TODO: Remove subset the train
+X_train, y_train = X_train[:100],  y_train[:100]
+
+
 X_test, y_test = test[:,1:], test[:,0]
 
-
-class kNN:
-    def __init__(self):
+cls = kNN(X=X_train, y=y_train)
+cls.predict(X_test[1], y_test[1])
