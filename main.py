a7cbd6b482eca049635190467e28e126007319be
Rob van Gastel <robvangastel@hotmail.com>
2020-03-03 11:04:24 +0100

    Comment LOOCV

--- main.py
+++ main.py
@@ -15,24 +15,25 @@ cls = kNN(X=X_train, y=y_train)
 y_hat = cls.predict(X_test)
 print("processing time: %.2f seconds" % (time.process_time() - start))
 
-def cross_validate(X, y, model):
-    loss = []
-    X_k = X
-    y_k = y
 
-    y_hat = []
-    for i in range(0, len(X)):
-        X_k = X[:i] + X[i+1:]
-        y_k = y[:i] + y[i+1:]
-        y_hat = model.predict(X_k)
-        # Default 0/1 loss
-        loss.append(np.sum(y_hat[:, i] != y_k))
+for i in range(0, 20):
+    print("0/1 loss: ", np.sum(y_hat[:, i] != y_test), "for k: ", i+1)
 
-        X_k = []
-        y_k = []
-    return loss
+# def cross_validate(X, y, model):
+#     loss = []
+#     X_k = X
+#     y_k = y
 
-cross_validate(X_test, y_test, cls)
+#     y_hat = []
+#     for i in range(0, len(X)):
+#         X_k = X[:i] + X[i+1:]
+#         y_k = y[:i] + y[i+1:]
+#         y_hat = model.predict(X_k)
+#         # Default 0/1 loss
+#         loss.append(np.sum(y_hat[:, i] != y_k))
 
-# for i in range(0, 20):
-#     print("0/1 loss: ", np.sum(y_hat[:, i] != y_test), "for k: ", i+1)
+#         X_k = []
+#         y_k = []
+#     return loss
+
+# cross_validate(X_test, y_test, cls)
