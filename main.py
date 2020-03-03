00ad8fbb06a0f31d45b5ce24e72443d07ae22484
Luc Antonissen <lucantonissen24@gmail.com>
2020-03-03 11:52:51 +0100

    Require distance function

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
 import numpy as np
 import time
 from kNN.kNN import kNN
+start = time.process_time()
 
 train = np.loadtxt(r'./data/MNIST_train_small.csv', 
-    delimiter=',')
+    delimiter=',')[:250]
 test = np.loadtxt(r'./data/MNIST_test_small.csv', 
-    delimiter=',')
+    delimiter=',')[:50]
 
 X_train, y_train = train[:,1:], train[:,0]
 X_test, y_test = test[:,1:], test[:,0]
 
-start = time.process_time()
 cls = kNN(X=X_train, y=y_train)
-y_hat = cls.predict(X_test)
-print("processing time: %.2f seconds" % (time.process_time() - start))
-
+y_hat = cls.predict(X_test, 'euclidian')
 
 for i in range(0, 20):
     print("0/1 loss: ", np.sum(y_hat[:, i] != y_test), "for k: ", i+1)
@@ -37,3 +35,5 @@ for i in range(0, 20):
 #     return loss
 
 # cross_validate(X_test, y_test, cls)
+
+print("processing time: %.2f seconds" % (time.process_time() - start))
